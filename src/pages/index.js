dfb36199241f83fd714dd2f819204a2b5495bb81
ivrunner <richyentam@gmail.com>
2019-01-10 10:22:57 -0500

    Gatsby image working and local build working.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import catAndHumanIllustration from '../images/cat-and-human-illustration.svg'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
-const IndexPage = () => (
+const IndexPage = props => (
   <Layout>
     <SEO
       title="Home"
@@ -15,15 +17,16 @@ const IndexPage = () => (
       <img
         src={catAndHumanIllustration}
         className="block mx-auto w-1/2"
-        alt="Cat and human sitting on a couch"
+        alt="Cat and human sitting on a bench"
       />
+      <Img fluid={props.data.imageOne.childImageSharp.fluid} />
 
       <h2 className="bg-yellow inline-block my-8 p-3">
         Hey there! Welcome to your first Gatsby site.
       </h2>
 
       <p className="leading-loose">
-        This is a barebones starter for Gatsby styled using{' '}
+        This is a barebones starter for Gatsby styled using{" "}
         <a
           href="https://tailwindcss.com/"
           className="font-bold no-underline text-grey-darkest"
@@ -34,6 +37,18 @@ const IndexPage = () => (
       </p>
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "one.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 3000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
