ce1958ddfc364d07315478f1f9051f0dc65db34d
ivrunner <richyentam@gmail.com>
2019-01-15 20:38:15 -0500

    started site build. still working on images in layout.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,66 +1,18 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
+
 import bklogo from "../images/bk-logo-01.svg";
-const handleClick = ev => {
-  ev.preventDefault();
-  const element = document.getElementById("nav");
-  element.classList.toggle("block");
-  element.classList.toggle("hidden");
-};
 
 const Header = ({ siteTitle }) => (
-  <nav className="bg-pink">
-    <div className="flex flex-wrap items-center justify-between max-w-3xl mx-auto px-8 py-3 md:px-8 md:py-3">
+  <nav className="bg-cover-image bg-repeat">
+    <div className="flex flex-wrap items-center justify-center max-w-3xl mx-auto px-8 py-3 md:px-8 md:py-3">
       <Link
         to="/"
-        className="flex items-center no-underline text-white w-1/3 sm:w-1/3 md:w-1/6"
+        className="flex items-center no-underline text-white w-1/2 sm:w-1/3 md:w-1/6"
       >
-        <img
-          src={bklogo}
-          className="block mx-auto"
-          alt="Cat and human sitting on a bench"
-        />
+        <img src={bklogo} className="" alt="Brook & Kyle Logo" />
       </Link>
-
-      <button
-        className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
-        onClick={handleClick}
-      >
-        <svg
-          className="fill-current h-3 w-3"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </button>
-
-      <div id="nav" className="hidden md:flex md:items-center w-full md:w-auto">
-        <div className="text-sm">
-          <Link
-            to="/"
-            className="block mt-4 md:inline-block md:mt-0 mr-6 no-underline text-white"
-          >
-            Home
-          </Link>
-
-          <Link
-            to="/about"
-            className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline text-white"
-          >
-            About
-          </Link>
-
-          <Link
-            to="/contact"
-            className="block md:inline-block mt-4 md:mt-0 no-underline text-white"
-          >
-            Contact
-          </Link>
-        </div>
-      </div>
     </div>
   </nav>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Img from "gatsby-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 const IndexPage = props => (
   <Layout>
@@ -15,44 +14,32 @@ const IndexPage = props => (
 
     <div className="text-center">
       <div class="flex flex-wrap">
-        <div class="w-full p-2">
-          <Img fluid={props.data.imageOne.childImageSharp.fluid} />
+        <div class="w-full border-4 border-bk-pink">
+          <Img
+            fluid={props.data.imageOne.childImageSharp.fluid}
+            class=""
+            alt="Brooke & Kyle"
+          />
         </div>
 
-        <div class="w-full md:w-1/2 p-2">
-          <p className="text-pink-darker">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum
-          </p>
+        <div class="w-full md:w-1/2 p-4">
+          <h1 class="text-bk-pink">Time & Date</h1>
+          <h2 class="text-bk-pink">Saturday July 1st, 2019</h2>
+          <p class="text-white">at 2 o'clock in the afternoon</p>
+        </div>
+        <div class="w-full md:w-1/2 p-4">
+          <h1 class="text-bk-pink">First Brethren Church</h1>
+          <h2 class="text-bk-pink">K of C Hall</h2>
+          <p class="text-white">13050 Williams County Rd C</p>
+          <p class="text-white">Bryan, Ohio 43506</p>
+        </div>
+        <div class="w-full md:w-1/2 p-4 pb-8">
+          <h1 class="text-bk-pink">Reception to follow at:</h1>
+          <h2 class="text-bk-pink">K of C Hall</h2>
+          <p class="text-white">111 Elliott Rd</p>
+          <p class="text-white">Defiance, Ohio 43512</p>
         </div>
       </div>
-      <img
-        src={catAndHumanIllustration}
-        className="block mx-auto w-1/2"
-        alt="Brooke and Kyle"
-      />
-
-      <h2 className="bg-yellow inline-block my-8 p-3">
-        Hey there! Welcome to your first Gatsby site.
-      </h2>
-
-      <p className="leading-loose">
-        This is a barebones starter for Gatsby styled using{" "}
-        <a
-          href="https://tailwindcss.com/"
-          className="font-bold no-underline text-grey-darkest"
-        >
-          Tailwind
-        </a>
-        , a utility-first CSS framework.
-      </p>
     </div>
   </Layout>
 );
@@ -61,7 +48,7 @@ export default IndexPage;
 
 export const pageQuery = graphql`
   query {
-    imageOne: file(relativePath: { eq: "unsplash-wedding.jpg" }) {
+    imageOne: file(relativePath: { eq: "bkpic.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1200) {
           ...GatsbyImageSharpFluid
