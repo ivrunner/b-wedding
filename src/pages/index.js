0ca03f6910f9a5fa445d6c21a19ee16b501d425b
ivrunner <richyentam@gmail.com>
2019-02-10 17:09:00 -0500

    testing form

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,29 +63,13 @@ const IndexPage = props => (
             </p>
             <p class="p-5">
               <label>
-                How Many?:{" "}
+                How Many?{" "}
                 <div class="inline-block relative w-64">
-                  <select class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                    <option>7</option>
-                    <option>8</option>
-                    <option>9</option>
-                    <option>10</option>
-                  </select>
-                  <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
-                    <svg
-                      class="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </div>
+                  <input
+                    class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    name="number of guests"
+                  />
                 </div>
               </label>
             </p>
